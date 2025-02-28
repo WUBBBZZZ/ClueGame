7f3aa783353fa35989c4fd9c8b3d119d0ca061f7
WUBBBZZZ <112722551+WUBBBZZZ@users.noreply.github.com>
2025-02-28 10:48:09 -0700

    updated TestBoard
    updated test board becuase it was not included in initial push

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -1,5 +1,20 @@
 package experiment;
 
-public class TestBoard {
+import java.util.Set;
 
+public class TestBoard {
+	String[] board;
+	
+	public TestBoard() {
+		return;
+	}
+	public void calcTargets(TestBoardCell startCell, int pathlength) {
+		return;
+	}
+	public TestBoardCell getCell(int row, int col) {
+		return null;
+	}
+	public Set<TestBoardCell> getTargets(){
+		return null;
+	}
 }
